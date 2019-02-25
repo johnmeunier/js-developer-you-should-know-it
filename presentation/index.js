b5656d4191a4943996f459515bc60e09604e5355
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2019-02-25 11:29:26 +0100

    array loc

--- presentation/index.js
+++ presentation/index.js
@@ -1,39 +1,39 @@
 // Import React
-import React from 'react';
-import { Deck, Heading, Slide } from 'spectacle';
-import createTheme from 'spectacle/lib/themes/default';
-import CodeSlide from 'spectacle-code-slide';
+import React from "react";
+import { Deck, Heading, Slide } from "spectacle";
+import createTheme from "spectacle/lib/themes/default";
+import CodeSlide from "spectacle-code-slide";
 
-import 'prismjs/themes/prism-okaidia.css';
-import './index.css';
+import "prismjs/themes/prism-okaidia.css";
+import "./index.css";
 
 const images = {
-  formidagon: require('../assets/formidable-logo.svg'),
-  goodWork: require('../assets/good-work.gif')
+  formidagon: require("../assets/formidable-logo.svg"),
+  goodWork: require("../assets/good-work.gif")
 };
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    mustard: '#FFC857',
-    ochre: '#E9724C',
-    cardinal: '#C5283D',
-    acajou: '#481D24',
-    blue: '#255F85',
-    white: '#fff'
+    mustard: "#FFC857",
+    ochre: "#E9724C",
+    cardinal: "#C5283D",
+    acajou: "#481D24",
+    blue: "#255F85",
+    white: "#fff"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica'
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={['zoom', 'slide']} transitionDuration={300} theme={theme} bgColor="mustard">
+      <Deck transition={["zoom", "slide"]} transitionDuration={300} theme={theme} bgColor="mustard">
         <Slide>
           <Heading size={2} textColor="acajou">
             JS Developer, you should know it
@@ -44,52 +44,27 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide>
           <Heading size={1} textColor="acajou">
-            {'{Object}'}
+            {"{Object}"}
           </Heading>
         </Slide>
         <CodeSlide
           className="codeSlide--john"
           lang="js"
-          transition={['fade']}
-          code={require('raw-loader!../assets/codes/object.example')}
-          ranges={[
-            {
-              loc: [0, 5],
-              title: 'Sample'
-            },
-            {
-              loc: [6, 7],
-              title: 'Entries',
-              note: '[["a", "somestring"], ["b", 42], ["c", false]]'
-            },
-            {
-              loc: [7, 8],
-              title: 'Keys',
-              note: '["a", "b", "c"]'
-            },
-            {
-              loc: [8, 9],
-              title: 'Values',
-              note: '["somestring", 42, false]'
-            }
-          ]}
+          transition={["fade"]}
+          code={require("raw-loader!../assets/codes/object.example")}
+          ranges={require("./loc/object.json")}
         />
         <Slide>
           <Heading size={1} textColor="acajou">
-            {'[Array]'}
+            {"[Array]"}
           </Heading>
         </Slide>
         <CodeSlide
           className="codeSlide--john"
           lang="js"
-          transition={['fade']}
-          code={require('raw-loader!../assets/codes/array.example')}
-          ranges={[
-            {
-              loc: [0, 9],
-              title: 'Sample'
-            }
-          ]}
+          transition={["fade"]}
+          code={require("raw-loader!../assets/codes/array.example")}
+          ranges={require("./loc/array.json")}
         />
       </Deck>
     );
